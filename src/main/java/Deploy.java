fac899db15f7aebe906cf4ab86abb3f72151e8bb
jsemenov <esemyonov@hortonworks.com>
2015-01-28 14:56:27 +0100

    adding new class

--- src/main/java/Deploy.java
+++ src/main/java/Deploy.java
@@ -5,8 +5,10 @@ public class Deploy {
 
      public static void main(String args []){
 
+        NewClass newClass = new NewClass();
 
-        
          System.out.println("Lets begin!");
+
+         System.out.println(newClass.getA());
      }
 }
---
+++ src/main/java/NewClass.java
@@ -0,0 +1,15 @@
+/**
+ * Created by user on 1/28/15.
+ */
+public class NewClass {
+
+    private int a = 10;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
